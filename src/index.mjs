fd0b17414414ebeeec6ea06a011b5f865d7f3cef
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-10-06 10:10:52 -0300

    feat: session storage|cart ex

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { response } from "express";
 import routes from './routes/index.mjs'
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser('helloworld'));
+app.use(session({
+   session: 'macharete rafael',
+   saveUninitialized: false, // Controls wheter a new, but unmodified, 
+   // session should be saved to the session store. False: when we don't want to save unmodified session data to the session store
+   resave: false,
+   cookie: {
+      maxAge: 60000 * 60,
+   },
+}));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +26,49 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (request, response) => {
+   console.log(request.session.id);
+   request.session.visited = true;
    response.cookie('hello', 'world', { maxAge: 10000, signed: true })
    response.status(200).send('<h1> Receiving cookie</h1>')
-}) 
+})
+
+app.post('/api/auth', (request, response) => {
+   const {
+      body: { username, password }
+   } = request;
+
+   const findUser = mockUsers.find((user) => user.username === username && user.password === password);
+   if (!findUser) return response.status(401).send({ msg: 'Enter valid credentials!' });
+
+   request.session.user = findUser;
+
+   response.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (request, response) => {
+   request.sessionStore.get(request.sessionID, (err, session) => {
+      console.log(session);
+   })
+
+   return request.session.user ?
+      response.status(200).send(request.session.user) :
+      response.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post('/api/cart', (request, response) => {
+   if (!request.session.user) return response.sendStatus(401);
+   const { body: item } = request;
+   const { cart } = request.session;
+   if (cart) { // If car is already defined, just push into.
+      cart.push(item);
+   } else { // If not, create it instance within session of request
+      request.session.cart = [item]
+   }
+
+   return response.status(201).send(item);
+}); 
+
+app.get('/api/cart', (request, response) => {
+   if (!request.session.user) return response.sendStatus(401);
+   return response.send(request.session.cart ?? []);
+})
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,10 +1,10 @@
 export const mockUsers = [
-   { id: 1, username: 'Rafael', displayName: 'Macharete' },
-   { id: 2, username: 'henry', displayName: 'Henry' },
-   { id: 3, username: 'steve', displayName: 'Steve' },
-   { id: 4, username: 'robin', displayName: 'Robin' },
-   { id: 5, username: 'ana', displayName: 'Ana' },
-   { id: 6, username: 'christine', displayName: 'Christine' },
-   { id: 7, username: 'ada', displayName: 'Ada' },
-   { id: 8, username: 'ada', displayName: 'Lovelace' },
+   { id: 1, username: 'Rafael', displayName: 'Macharete', password: '123'},
+   { id: 2, username: 'henry', displayName: 'Henry', password: '321'},
+   { id: 3, username: 'steve', displayName: 'Steve', password: '111'},
+   { id: 4, username: 'robin', displayName: 'Robin', password: '222'},
+   { id: 5, username: 'ana', displayName: 'Ana', password: '333'},
+   { id: 6, username: 'christine', displayName: 'Christine', password: '4'},
+   { id: 7, username: 'ada', displayName: 'Ada', password: '5'},
+   { id: 8, username: 'ada', displayName: 'Lovelace', password: '6'},
 ]
